54f0cbded1c2b8f86ee6bb09190dbd5d7776c61f
MateusZurdoSierra <mateus.zurdo@superlogica.com>
2024-10-22 16:38:17 -0300

    remove console

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-console.log("APIKEY FIRESTORE", process.env);
-
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
